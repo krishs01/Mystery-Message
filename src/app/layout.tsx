45f5961dd69132844027f9bb7f87f3642e8c1499
Krish Singh 01 <krishkumarsingh2003@gmail.com>
2024-07-19 10:59:57 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import AuthProvider from "@/context/AuthProvider";
+import { Toaster } from "@/components/ui/toaster"
+import { useToast } from "@/components/ui/use-toast"
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +20,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <AuthProvider>
+
+      <body className={inter.className}>
+        <Navbar />           
+      {children}
+      <Toaster />
+      </body>
+      </AuthProvider>
     </html>
   );
 }
