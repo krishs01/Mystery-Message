e4bd9b688251f537f5827bc36147d45c8a86ad68
aryan262 <aryan.nikolas@gmail.com>
2024-08-07 20:31:34 +0530

    "Update .env file and dbConnect.ts: removed RESEND_API_KEY, updated MONGODB_URI, and added error handling for MONGODB_URI environment variable"

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,4 +1,3 @@
-import { log } from "console";
 import mongoose from "mongoose";
 
 type ConnectionObject = {
@@ -13,19 +12,23 @@ async function dbConnect(): Promise<void>
     if(connection.isConnected)
     {
         console.log('Already connected to Database')
-        return 
+        return; 
     }
-
+    if (!process.env.MONGODB_URI) {
+        console.error('MONGODB_URI is not set');
+        // process.exit(1);
+    }
     try {
+        // console.log(process.env.MONGODB_URI)
         const db = await mongoose.connect(process.env.MONGODB_URI! || '', {})
 
         connection.isConnected = db.connections[0].readyState
 
-        console.log(db, "DB connected Successfully");
+        console.log("DB connected Successfully");
 
     } catch (error: any) {
-        console.log('DataBase connection failed', error);
-        process.exit(1);        
+        console.log('DataBase connection failed', error.message);
+        // process.exit(1);        
     }
 }
 
